174556dadfb4c54f7c7c8e04800ffc1c27f7ec5f
Martin-Martinez-4 <martmartez4@gmail.com>
2025-07-03 21:16:36 -0700

    Added a currentPageMEdiator class that is used to handle displaying the correct page.  Added some content to two pages (needs more styling).  Might try to implement web components later.

--- concept/script.js
+++ concept/script.js
@@ -1,42 +1,60 @@
-const $ = document.querySelectorAll
-
-
 
 const pages = document.querySelectorAll('.page')
+const pagesLength =  pages.length
+
+class CurrentPageMediator{
+  constructor(pages, currentPage = 0){
+
+    this.pages = pages
+    this.currentPage = currentPage
+  }
+
+  setCurrentPage(number){
+    const lastPage = this.currentPage
+    this.currentPage = number
+
+    console.log(lastPage)
+    console.log(this.currentPage)
+
+    pages[lastPage].classList.toggle('is-visible');
+    pages[this.currentPage].classList.toggle('is-visible');
+  }
+}
+
+const currentPageMediator = new CurrentPageMediator(pages, 0)
 
 function handleClick(currentPage){
   currentPage.style.zIndex = 1
   currentPage.classList.toggle('is-visible')
 }
 
-pagesLength =  pages.length
-
 pages[0].classList.toggle('is-visible')
 
 for(let p = 0; p < pagesLength-1; p++ ){
-  const prevPage = pages[p -1]
   const currentPage = pages[p]
-  const nextPage = pages[p +1]
 
 
-  nextPageButton = currentPage.querySelector(".button__next")
-  prevPageButton = currentPage.querySelector(".button__prev")
+  const nextPageButton = currentPage.querySelector(".button__next")
+  const prevPageButton = currentPage.querySelector(".button__prev")
 
   if(p == 0){
     prevPageButton.classList.toggle("fade-in-element")
   }
 
   nextPageButton.addEventListener("click", (e)=> {
-    console.log("clicked")
-    currentPage.classList.toggle('is-visible'); 
-    nextPage.classList.toggle('is-visible');
+    currentPageMediator.setCurrentPage(p+1)
   })
 
   prevPageButton.addEventListener("click", (e) => {
-    currentPage.classList.toggle('is-visible'); 
-    prevPage.classList.toggle('is-visible');
+    currentPageMediator.setCurrentPage(p-1)
   })
 
-
 }
+
+document.addEventListener("keyup", (e)=>{
+  if(e.code == "Space"){
+    e.preventDefault()
+    currentPageMediator.setCurrentPage(currentPageMediator.currentPage+1)
+  }
+})
 
