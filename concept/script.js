54ebbe880e5d146f1ffa2821930ce26699f461dc
Martin-Martinez-4 <martmartez4@gmail.com>
2025-07-14 21:20:56 -0700

    Started to migrate to web components; Made presentation-page and prev-next-buttons components.

--- concept/script.js
+++ concept/script.js
@@ -1,20 +1,61 @@
 
-const pages = document.querySelectorAll('.page')
-const pagesLength =  pages.length
+class PresentationPage extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
 
-class CurrentPageMediator{
-  constructor(pages, currentPage = 0){
+class PrevNextButtons extends HTMLElement {
+  constructor() {
+    super();
+
+    this.classList.add("buttons")
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.innerHTML = `
+    <style>
+      .button{
+        padding: .5rem 1rem;
+        text-align: center;
+        border-radius: 5px;
+        opacity: .8;
+        background: inherit;
+        width: 4rem;
+        height: 2rem;
+        line-height: 2rem;
+        color: white;
+        border: 1px solid whitesmoke;
+        box-shadow: black 1px 1px 2px;
+        cursor: pointer;
+
+      }
+
+      .button__prev{
+        margin-left: 1rem;
+      }
+
+      .button__next{
+        margin-right: 1rem;
+      }
+    </style>
+      <div class="button button__prev">prev</div>
+      <div class="button button__next">next</div>
+    `
+  }
+}
+
+class CurrentPageMediator {
+  constructor(pages, currentPage = 0) {
 
     this.pages = pages
     this.currentPage = currentPage
   }
 
-  setCurrentPage(number){
-    
-    if(number > pagesLength-1){
+  setCurrentPage(number) {
+
+    if (number > pagesLength - 1) {
       return
     }
-    if(number < 0){
+    if (number < 0) {
       return
     }
     const lastPage = this.currentPage
@@ -24,47 +65,55 @@ class CurrentPageMediator{
   }
 }
 
+customElements.define("presentation-page", PresentationPage)
+customElements.define("prev-next-buttons", PrevNextButtons)
+
+const pages = document.querySelectorAll('presentation-page')
+const pagesLength = pages.length
+
+
 const currentPageMediator = new CurrentPageMediator(pages, 0)
 
-function handleClick(currentPage){
+function handleClick(currentPage) {
   currentPage.style.zIndex = 1
   currentPage.classList.toggle('is-visible')
 }
 
 pages[0].classList.toggle('is-visible')
 
-for(let p = 0; p < pagesLength; p++ ){
+
+for (let p = 0; p < pagesLength; p++) {
   const currentPage = pages[p]
 
+  const buttons = currentPage.querySelector(".buttons")
+  const nextPageButton = buttons.shadowRoot.querySelector(".button__next")
+  const prevPageButton = buttons.shadowRoot.querySelector(".button__prev")
 
-  const nextPageButton = currentPage.querySelector(".button__next")
-  const prevPageButton = currentPage.querySelector(".button__prev")
-
-  if(p == 0){
+  if (p == 0) {
     prevPageButton.classList.toggle("opacity-0")
     prevPageButton.classList.toggle("pointer-events-none")
 
   }
 
-  if(p == pagesLength-1){
+  if (p == pagesLength - 1) {
     nextPageButton.classList.toggle("fade-in-element")
 
   }
 
-  nextPageButton.addEventListener("click", (e)=> {
-    currentPageMediator.setCurrentPage(p+1)
+  nextPageButton.addEventListener("click", (e) => {
+    currentPageMediator.setCurrentPage(p + 1)
   })
 
   prevPageButton.addEventListener("click", (e) => {
-    currentPageMediator.setCurrentPage(p-1)
+    currentPageMediator.setCurrentPage(p - 1)
   })
 
 }
 
-document.addEventListener("keyup", (e)=>{
-  if(e.code == "Space"){
+document.addEventListener("keyup", (e) => {
+  if (e.code == "Space") {
     e.preventDefault()
-    currentPageMediator.setCurrentPage(currentPageMediator.currentPage+1)
+    currentPageMediator.setCurrentPage(currentPageMediator.currentPage + 1)
   }
 })
 
